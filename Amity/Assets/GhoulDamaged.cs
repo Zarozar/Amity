ba5fd2165ab3e4fdd28b7d63bef3f9b99fb5fe70
RIPjaa <71727400+RIPjaa@users.noreply.github.com>
2020-09-30 23:26:08 +0700

    Ghoul

---
+++ Amity/Assets/GhoulDamaged.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GhoulDamaged : MonoBehaviour
+{
+    public GameObject Enemy;
+
+    private GhoulWalker GhoulScript;
+
+    private int counter = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        GhoulScript = Enemy.GetComponent<GhoulWalker>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public virtual void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.GetComponent<Player>() != null && other.GetComponent<Player>().IsDead == false)
+        {
+            other.GetComponent<Character>().TakeDamage(GhoulScript.damage);
+        }
+    }
+
+}
---
+++ Amity/Assets/GhoulWalker.cs
@@ -0,0 +1,98 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GhoulWalker : MonoBehaviour
+{
+    public GameObject Target { get; set; }
+
+    public int damage;
+
+    [SerializeField]
+    private float speed;
+    
+    private bool facingRight;
+    
+    [SerializeField]
+    private int health;
+    
+    private Animator animator;
+    public bool IsDead
+    {
+        get
+        {
+            return health <= 0;
+        }
+    }
+
+    public GameObject Explode;
+
+    public void Start()
+    {
+        damage = 10;
+    }
+
+    private void Update()
+    {
+
+        if (!IsDead)
+        {
+            LookAtTarget();
+            Move();
+            transform.Translate(Vector2.left * speed * Time.deltaTime);
+        }
+
+    
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.gameObject.tag == "Sword")
+        {
+            Destroy(other.gameObject);
+            StartCoroutine(SecondDeath(0.2f));
+        }
+    }
+
+    IEnumerator SecondDeath(float sec)    
+    {
+        yield return new WaitForSeconds(sec);
+        Instantiate(Explode, transform.position, transform.rotation);
+        Destroy(this.gameObject);
+    }
+    private void LookAtTarget()
+    {
+        if (Target != null)
+        {
+            float xDir = Target.transform.position.x - transform.position.x;
+
+            if (xDir < 0 && facingRight || xDir > 0 && !facingRight)
+            {
+                ChangeDirection();
+            }
+        }
+    }
+    public Vector2 GetDirection()
+    {
+        return facingRight ? Vector2.right : Vector2.left;
+    }
+    public void ChangeDirection()
+    {
+        facingRight = !facingRight;
+        transform.localScale = new Vector3(transform.localScale.x * -1f, transform.localScale.y, transform.localScale.z);
+    }
+
+    public void Move()
+    {
+        if (Target != null)
+        {
+            animator.SetFloat("speed", 1);
+
+            transform.Translate(GetDirection() * (speed * Time.deltaTime));
+        }
+    }
+         
+
+
+    
+}
