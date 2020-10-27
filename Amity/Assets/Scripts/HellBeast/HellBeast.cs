eab419456a1bce361888cae72879adbfcd28270c
Zarozar <nathan25452002@gmail.com>
2020-10-28 02:09:28 +0700

    hell beast reboot

---
+++ Amity/Assets/Scripts/HellBeast/HellBeast.cs
@@ -0,0 +1,98 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HellBeast : MonoBehaviour
+{
+    public GameObject Target { get; set; }
+
+    [SerializeField]
+    private float ShotRange;
+
+    public Animator animator { get; private set; }
+
+    public bool InShotRange
+    {
+        get
+        {
+            if (Target != null)
+            {
+                return Vector2.Distance(transform.position, Target.transform.position) <= ShotRange;
+            }
+
+            return false;
+        }
+    }
+    public bool IsDead
+    {
+        get
+        {
+            return health <= 0;
+        }
+    }
+
+    private bool facingRight = false;
+
+    [SerializeField]
+    public int damage { get; set; }
+
+    [SerializeField]
+    protected int health;
+
+
+    private void Start()
+    {
+        damage = 10;
+        animator = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        if (!IsDead)
+        {
+            if (InShotRange)
+            {
+                animator.SetTrigger("attack");
+            }
+        }
+        else
+        {
+            animator.SetTrigger("death");
+        }
+        LookAtTarget();
+        
+        
+    }
+
+    private void LookAtTarget()
+    {
+        if (Target != null)
+        {
+            float xDir = Target.transform.position.x - transform.position.x;
+
+            if (xDir < 0 && facingRight || xDir > 0 && !facingRight)
+            {
+                ChangeDirection();
+            }
+        }
+    }
+    public Vector2 GetDirection()
+    {
+        return facingRight ? Vector2.right : Vector2.left;
+    }
+    private void ChangeDirection()
+    {
+        facingRight = !facingRight;
+        transform.localScale = new Vector3(transform.localScale.x * -1f, transform.localScale.y, transform.localScale.z);
+    }
+    public void HellBeastDeath(float sec)
+    {
+        StartCoroutine(H_SecondDeath(sec));
+    }
+
+    IEnumerator H_SecondDeath(float sec)
+    {
+        yield return new WaitForSeconds(sec);
+        Destroy(this.gameObject);
+    }
+}
