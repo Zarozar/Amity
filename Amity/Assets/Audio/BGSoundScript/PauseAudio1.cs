3607217bf11de322d30e85d7cc092ac331a2f489
RIPjaa <71727400+RIPjaa@users.noreply.github.com>
2020-11-04 18:53:42 +0700

    BgSound

---
+++ Amity/Assets/Audio/BGSoundScript/PauseAudio1.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseAudio1 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        BGSoundScript1.Instance.gameObject.GetComponent<AudioSource>().Pause();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
+
